cf7e90e6b96389190336d33a78299a252a8fa66f
Anthony Horton <anthony.horton@aao.gov.au>
2018-09-13 16:30:23 +1000

    Initial working implementation

--- README.md
+++ README.md
@@ -1,2 +1,57 @@
 # velocecal-shutter
 Python script for control of the VeloceCal shutter via the temporary Raspberry Pi set up.
+
+## Installing
+
+### With git
+
+```
+git clone https://github.com/AnthonyHorton/velocecal-shutter.git
+cd velocecal-shutter
+pip install -r requirements.txt
+```
+
+If not using a conda/pyvenv/virtualenv environment then add the `--user` option to the `pip install`
+step.
+
+### Without git
+
+```
+wget https://github.com/AnthonyHorton/velocecal-shutter/archive/master.zip
+unzip velocecal-shutter-master.zip
+cd velocecal-shutter
+pip install -r requirements.txt
+```
+
+Then link to `shutter-run.py` from somewhere in your path, e.g. `sudo ln -s shutter-run.py /usr/local/bin/`.
+
+
+## Running
+
+Default operation is to start immediately, and open the shutter twice, once at the start and once at the end of the sequence. To do this just specify the total shutter open time and total elapsed time,
+both in seconds.
+
+```
+shutter-run.py <total shutter open time> <total elapsed time>
+```
+
+There are options to delay the start of the sequence, change the number of times the shutter opens,
+and change the hostname/IP of the Raspberry Pi.  For a fill list of arguments/options
+```
+shutter-run.py --help
+```
+
+
+## Updating
+
+### With git
+
+```
+cd velocecal-shutter
+git pull
+```
+
+
+### Without git
+
+Reinstall as above.
---
+++ shutter-run.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python3
+import argparse
+import time
+import os
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Script to operate the VeloceCal shutter')
+    parser.add_argument('--nopen', '-n',
+                        help='Number of times to open the shutter',
+                        type=int,
+                        default=2)
+    parser.add_argument('--delay', '-d',
+                        help='Delay, in seconds, before starting the shutter sequence',
+                        type=float,
+                        default=0)
+    parser.add_argument('--host',
+                        help='Hostname or IP address of the Raspberry Pi controling the shutter',
+                        default='10.88.21.151')
+    parser.add_argument('opentime',
+                        help='Total time to keep the shutter open for, in seconds',
+                        type=float)
+    parser.add_argument('totaltime',
+                        help='Total elapsed time for the shutter opening sequence, in seconds',
+                        type=float)
+    args = parser.parse_args()
+
+    # Need to set these environment variables otherwise the imports fail
+    os.environ['GPIOZERO_PIN_FACTORY'] = 'pigpio'
+    os.environ['PIGPIO_ADDR'] = args.host
+
+    # Need to import down here because they fail if done before setting the environment variables
+    from gpiozero import DigitalOutputDevice
+    from gpiozero.pins.pigpio import PiGPIOFactory
+
+    # Connect to GPIO 27 on the Raspberry Pi
+    remote_pin_factory = PiGPIOFactory(host=args.host)
+    shutter = DigitalOutputDevice(pin=27, pin_factory=remote_pin_factory)
+
+    exposure = args.opentime / args.nopen
+    wait_time = (args.totaltime - args.opentime) / (args.nopen - 1)
+
+    time.sleep(args.delay)
+    shutter.on()
+    time.sleep(exposure)
+    shutter.off()
+    for i in range(args.nopen - 1):
+        time.sleep(wait_time)
+        shutter.on()
+        time.sleep(exposure)
+        shutter.off()
+
+    shutter.close()
